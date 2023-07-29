222eb5ce62cfadfc95f8b811a137614155bff5a3
cesar <sezaruuufreire@outlook.com>
2023-07-29 15:01:21 -0300

    adicionando o Todo:

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,6 +1,12 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println();
-        System.out.println("Hello, World!");
+        //Todo: conhecer e importar a classe Scanner
+
+        //Exibir as mensagens para o nosso usuário
+
+        //Obter pela scanner os valores digitados
+
+        //Exibir a mensagem conta criada
+
     }
 }
